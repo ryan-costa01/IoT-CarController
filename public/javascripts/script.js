17c28d9392ba4a17f2e029cd7a06ad8d555d9dfc
ryan-costa01 <ryanfellipe2001@usp.br>
2024-06-25 08:20:10 -0300

    Config site

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   connectButton.addEventListener('click', function() {
     if (!isConnected) {
-      socket = new WebSocket('ws://localhost:3000');
+      socket = new WebSocket('ws://vibaryje.igbt.eesc.usp.br');
       socket.binaryType = 'arraybuffer';
 
       socket.addEventListener('open', function() {
