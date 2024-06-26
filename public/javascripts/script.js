a579429addc9b18b304195b9b0190d537ca7bcfa
ryan-costa01 <ryanfellipe2001@usp.br>
2024-06-25 21:15:56 -0300

    teste websocket4

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   connectButton.addEventListener('click', function() {
     if (!isConnected) {
-      socket = new WebSocket('ws://localhost:6004');
+      socket = new WebSocket('ws://vibaryje.igbt.eesc.usp.br:6004');
       socket.binaryType = 'arraybuffer';
 
       socket.addEventListener('open', function() {
