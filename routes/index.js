e9779a312cfb2d5b36b26c2fd527b1ae728fd1a7
Jessé <84997702+jessejj2000@users.noreply.github.com>
2024-06-26 16:20:33 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ router.post('/', function(req, res, next) {
     if(currentData.direction != currentdData.prevDirection){
       publishToMQTT('jesse', req.body.direction);
       currentData.prevDirection = currentData.direction; 
-      console.log(currentData.prevDirection)
+      console.log(currentData.prevDirection);
     }
   }else{
     publishToMQTT('jesse', "NONE");
